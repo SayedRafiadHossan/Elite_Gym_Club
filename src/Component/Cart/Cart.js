5c1dfff63d96e369abc2218ecd739d6853932850
Rafiad <rafiadhoseen@gmail.com>
2022-09-29 21:19:37 +0600

    Responsive done

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -21,7 +21,7 @@ const Cart = (props) => {
         addCompleteToast.classList.add('hidden');
     }
     return (
-        <nav>
+        <nav className=' card-contain'>
         <div className='flex'>
             <img class="inline-block h-12 w-12 rounded-full ring-2 ring-white my-5 mx-10" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
             <div className='my-5'>
