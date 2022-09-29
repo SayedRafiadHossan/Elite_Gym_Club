3c8c3a5f625ab54f84b1a478d35d75858164bdbd
Rafiad <rafiadhoseen@gmail.com>
2022-09-29 22:17:56 +0600

    Bug fixed

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -38,25 +38,3 @@ const Header = () => {
     );
 };
 export default Header;
-
-
-        <nav class="p-3 bg-gray-50 rounded border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-            <div class="container flex flex-wrap justify-between items-center mx-auto">
-                <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500" aria-controls="navbar-solid-bg" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                </button>
-            </div>
-            <div className='header'>
-            <div className='header-right-site'>
-                <img src={logo} alt="" />
-                <h1>Elite-Gym-Club</h1>
-            </div>
-            <div className='nav-right'>
-                <a href="/Home">Home</a>
-                <a href="/Exercise">Exercise</a>
-                <a href="/Activity">Activity</a>
-                <a href="/about">About</a>
-            </div> 
-            </div>
-        </nav>
